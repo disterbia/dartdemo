1cb59bd164ea948b9cee72fa47808974c656f582
이지원 <jwlee@ijiwon-ui-MacBookPro.local>
2020-11-27 11:17:32 +0900

    합치는중

---
+++ lib/player.dart
@@ -0,0 +1,209 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_midi_example/model/song.dart';
+import 'package:flutter_sequencer/sequence.dart';
+import 'package:flutter_sequencer/instrument.dart';
+import 'package:dart_midi/dart_midi.dart';
+import 'package:flutter/services.dart';
+
+Song midiToTracks(String path, Sequence seq) {
+  int ticksPerBit = 480;
+  int nowBPM = 120;
+  List<MidiNote> recorderMelodyArr = [];
+  int recorderLineIndex = -1;
+  bool isRhythmSet = false;
+  bool isBpmSet = false;
+  Song song = Song(
+      id: 0,
+      title: "Untitled",
+      tempo: 120,
+      notes: [],
+      rhythmUnder: 4,
+      rhythmUpper: 4);
+
+  try {
+    rootBundle.load(path).then((value) {
+      var parsedMidi = MidiParser().parseMidiFromBuffer(
+          value.buffer.asUint8List(value.offsetInBytes, value.lengthInBytes));
+      List<Instrument> instruments = [];
+
+      var parsedMidiTracks = parsedMidi.tracks;
+      int parsedMidiTracksCnt = parsedMidiTracks.length;
+
+      /*
+       * 미디분석 (트랙 갯수, 트랙악기명)
+       */
+      for (int i = 0; i < parsedMidiTracksCnt; i++) {
+        Sf2Instrument sf2Inst = null;
+        parsedMidiTracks[i].forEach((midiEvent) {
+          if (midiEvent is TrackNameEvent) {
+            String instName = (midiEvent as TrackNameEvent).text;
+            if (instName.toLowerCase().contains("recorder")) {
+              recorderLineIndex = i;
+              sf2Inst =
+                  Sf2Instrument(path: "assets/Recorder.sf2", isAsset: true);
+            }
+          } else if (midiEvent is TimeSignatureEvent && !isRhythmSet) {
+            TimeSignatureEvent tem = midiEvent;
+            song.rhythmUpper = tem.numerator;
+            song.rhythmUnder = tem.denominator;
+          } else if (midiEvent is SetTempoEvent && !isBpmSet) {
+            SetTempoEvent tem = midiEvent;
+            song.tempo = (60000000 / tem.microsecondsPerBeat).round();
+          }
+
+          // ppp1 pp4 p1 GrandPiano
+          if (sf2Inst == null) {
+            sf2Inst = Sf2Instrument(path: "assets/sf2/ppp1.sf2", isAsset: true);
+          }
+        });
+        instruments.add(sf2Inst);
+      }
+
+      /*
+       * 틱스퍼비트
+       */
+      ticksPerBit = parsedMidi.header.ticksPerBeat;
+
+      /*
+       * 앱 내 플레이어(시퀀서) 트랙만들기 시작
+       */
+      seq.createTracks(instruments).then((realTracks) {
+        int trackIndex = 0;
+
+        /*
+         * 파싱된 미디를 분석 후 플레이어 트랙으로 만듦
+         */
+        parsedMidiTracks.forEach((midiTrack) {
+          List<int> pitchs = [];
+          List<double> startPositions = [];
+          List<int> velocitys = [];
+          List<double> durations = [];
+
+          double pos = 0;
+          int originPos = 0;
+
+          midiTrack.forEach((element) {
+            if (element is ControllerEvent ||
+                element is NoteOnEvent ||
+                element is NoteOffEvent) {
+              originPos = originPos + element.deltaTime;
+              pos = pos + (element.deltaTime * (120 / nowBPM));
+            }
+
+            if (element is NoteOnEvent) {
+              pitchs.add(element.noteNumber);
+              velocitys.add(element.velocity);
+              startPositions.add(pos);
+            }
+            if (element is NoteOffEvent) {
+              double tem = element.deltaTime == 0
+                  ? durations[durations.length - 1]
+                  : element.deltaTime * (120 / nowBPM);
+              durations.add(tem);
+            }
+          });
+
+          if (pitchs.length > 0) {
+            int index = pitchs.length;
+            for (var i = 0; i < index; i++) {
+              realTracks[trackIndex].addNote(
+                  noteNumber: trackIndex == recorderLineIndex
+                      ? pitchs[i] - 12
+                      : pitchs[i],
+                  velocity: (velocitys[i] / 100) > 1 ? 1 : velocitys[i] / 100,
+                  startBeat: startPositions[i] / ticksPerBit,
+                  durationBeats: trackIndex == recorderLineIndex
+                      ? durations[i] / ticksPerBit
+                      : durations[i] / ticksPerBit * 1.5);
+              if (trackIndex == recorderLineIndex) {
+                recorderMelodyArr.add(MidiNote(
+                    pitch: pitchs[i],
+                    startPosition: startPositions[i] / ticksPerBit * 1000,
+                    dulation:
+                        ((durations[i] / ticksPerBit) * 100).ceil() * 10));
+              }
+            }
+          }
+          if (trackIndex != recorderLineIndex) {
+            realTracks[trackIndex].addVolumeChange(volume: 1, beat: 0);
+          } else {
+            realTracks[trackIndex].addVolumeChange(volume: 0.7, beat: 0);
+          }
+          trackIndex++;
+        });
+
+        song.notes = midiNoteToNote(
+            recorderMelodyArr, song.rhythmUpper, song.rhythmUnder);
+      });
+    });
+    return song;
+  } catch (e) {
+    print("fuck");
+  }
+}
+
+List<Note> midiNoteToNote(
+    List<MidiNote> midiNote, int rhythmUpper, int rhythmUnder) {
+  List<Note> notes = [];
+  int maxLengthOfMadi = (4000 / rhythmUnder).round();
+  maxLengthOfMadi *= rhythmUpper;
+  int position = 0;
+  midiNote.forEach((note) {
+    if (position == note.startPosition) {
+      notes.add(Note(leng: note.dulation, pitch: note.pitch, state: 0));
+      position += note.dulation;
+    } else {
+      //쉼표처리
+      List<int> restNotes = [4000, 3000, 2000, 1500, 1000, 750, 500, 375, 250];
+      int restLength = note.startPosition.toInt() - position;
+
+      restNotes.forEach((nowRest) {
+        if (maxLengthOfMadi >= nowRest) {
+          int cnt = (restLength / nowRest).floor();
+          if (cnt > 0) {
+            restLength -= nowRest * cnt;
+            for (int i = 0; i < cnt; i++) {
+              notes.add(Note(leng: nowRest, pitch: -1, state: 0));
+            }
+          }
+        }
+      });
+
+      //쉼표 후 노트삽입
+      notes.add(Note(leng: note.dulation, pitch: note.pitch, state: 0));
+      position += note.dulation + note.startPosition.toInt();
+    }
+  });
+  return notes;
+}
+
+void test(List<Note> notes) {
+  int maxLengthOfMadi = 4000;
+
+  List<int> restNotes = [4000, 3000, 2000, 1500, 1000, 750, 500, 375, 250];
+  int restLength = 7750;
+
+  restNotes.forEach((nowRest) {
+    if (maxLengthOfMadi >= nowRest) {
+      int cnt = (restLength / nowRest).floor();
+      if (cnt > 0) {
+        restLength -= nowRest * cnt;
+        for (int i = 0; i < cnt; i++) {
+          notes.add(Note(leng: nowRest, pitch: -1, state: 0));
+        }
+      }
+    }
+  });
+}
+
+/*
+ * 미디파일 파싱중 쓰는 노트객체
+ */
+
+class MidiNote {
+  int pitch;
+  double startPosition;
+  int dulation = 0; // 400 온음표 / 200 2분음표 / 100 4분음표 / 50 8분음표 / 25 16분음표
+
+  MidiNote({this.pitch, this.startPosition, this.dulation});
+}
---
+++ lib/shit_music.dart
@@ -0,0 +1,279 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_midi_example/model/song.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_midi_example/noteParser.dart';
+
+//한줄에 허용할 음표 갯수 정하고 거기에 맞게 마디 만들고 create 마디 해주기
+ListView createSong(int maxNotesInLine, Song song, BuildContext context) {
+  //한마디에 총 허용되는 노트(1000 = 4분음표 1개)
+  var maxOfMadi = 4000 * song.rhythmUpper / song.rhythmUnder;
+  List<Madi> madiList = [];
+  madiList.add(Madi(
+      isRhythmShown: false,
+      endType: 0,
+      clef: 0,
+      scale: 0,
+      rhythmUnder: song.rhythmUnder,
+      rhythmUpper: song.rhythmUpper,
+      notes: []));
+
+  // 1. 노래를 마디 리스트화
+  int nowMadiLength = 0;
+  song.notes.forEach((note) {
+    if (nowMadiLength + note.leng <= maxOfMadi) {
+      madiList.last.notes.add(note);
+      nowMadiLength = nowMadiLength + note.leng;
+    } else {
+      madiList.add(Madi(
+          isRhythmShown: false,
+          endType: 0,
+          clef: 0,
+          scale: 0,
+          rhythmUnder: song.rhythmUnder,
+          rhythmUpper: song.rhythmUpper,
+          notes: []));
+      madiList.last.notes.add(note);
+      nowMadiLength = note.leng;
+    }
+  });
+
+  List<Row> rows = [];
+  var deviceWidth = MediaQuery.of(context).size.width;
+
+  print(madiList.length);
+
+  // 줄별로 마디 분배
+  int nowNoteCnt = 0;
+  int madiCount = 0;
+  int madiTotalSize = madiList.length;
+  var tempMadisList = [];
+  var temp = [];
+  madiList.forEach((madi) {
+    madiCount++;
+    print(
+        "$madiCount $nowNoteCnt ${madi.getNotesCount()} ${madiList.length} $maxNotesInLine");
+    if (madi.getNotesCount() >= maxNotesInLine) {
+      if (temp.length > 0) {
+        tempMadisList.add(temp);
+        temp = [];
+      }
+      tempMadisList.add([madi]);
+      nowNoteCnt = 0;
+    } else {
+      if (nowNoteCnt + madi.getNotesCount() > maxNotesInLine) {
+        tempMadisList.add(temp);
+        temp = [madi];
+        nowNoteCnt = madi.getNotesCount();
+      } else {
+        temp.add(madi);
+        nowNoteCnt = nowNoteCnt + madi.getNotesCount();
+      }
+
+      if (madiCount == madiTotalSize) {
+        tempMadisList.add(temp);
+      }
+    }
+  });
+
+  // tempMadisList.forEach((element) {
+  //   print(element.length);
+  // });
+  // 줄별로 분배된 마디를 위젯으로 만들고 Row에 삽입
+  int tempCnt = 0;
+  int tempMadisListCnt = tempMadisList.length;
+  tempMadisList.forEach((madis) {
+    tempCnt++;
+    int cnt = madis.length;
+    double firstWidth = cnt > 1 ? deviceWidth / cnt * 1.17 : deviceWidth;
+    double otherWidth =
+        cnt > 1 ? (deviceWidth - firstWidth) / (cnt - 1) : deviceWidth;
+    List<Widget> temRowItem = [];
+    for (int i = 0; i < cnt; i++) {
+      double widthSize = 0;
+      if (i == 0) {
+        madis[i].clef = 1;
+        madis[i].isRhythmShown = true;
+        madis[i].endType = cnt == 1 && tempCnt == tempMadisListCnt ? 2 : 0;
+        widthSize = cnt == 1 && tempCnt == tempMadisListCnt
+            ? firstWidth / maxNotesInLine * madis[i].getNotesCount()
+            : firstWidth;
+      } else {
+        widthSize = otherWidth;
+      }
+      temRowItem.add(createMadi(madis[i], widthSize));
+    }
+    rows.add(Row(
+      children: temRowItem,
+    ));
+  });
+
+  // 2. 마디 리스트 아이템의 노트 갯수와 한줄에 허용되는 노트의 갯수와 맞게 Row List화
+  ListView listView = ListView(
+    children: rows,
+  );
+
+  // 3. 컨테이너 return
+  return listView;
+}
+
+class Madi {
+  bool isRhythmShown; //  곡시작여부(박자표)
+  int endType; // 0 일반, 1  도돌이표, 2, :곡끝
+  int clef; // 1 : 높은음자리표 -1 : 낮은음자리표 0 : 없음
+  int scale; // 0 : CMajor 0~7 : #갯수 -1~-7 : b갯수
+  int rhythmUnder;
+  int rhythmUpper;
+  List<Note> notes;
+  Madi(
+      {@required this.isRhythmShown,
+      @required this.endType,
+      @required this.clef,
+      @required this.scale,
+      @required this.rhythmUnder,
+      @required this.rhythmUpper,
+      @required this.notes});
+  int getNotesCount() {
+    int size = 0;
+    notes.forEach((note) {
+      size = size + 1;
+    });
+    return size;
+  }
+
+  int getNotesTotalSize() {
+    int size = 0;
+    notes.forEach((note) {
+      size = size + note.leng;
+    });
+    return size;
+  }
+}
+
+//음자리표 그려주는 함수
+Widget drawClef(bool isTreble, widgetHeight) {
+  return Positioned(
+    child: SvgPicture.asset(
+      isTreble ? "assets/start.svg" : "assets/start.svg",
+      height: widgetHeight * 0.7,
+    ),
+    height: widgetHeight,
+    left: widgetHeight / 15,
+  );
+}
+
+//박자표 그려주는 함수
+List<Widget> drawRhythem(int upper, int under, double widgetHeight) {
+  List<Widget> list = [];
+  list.add(Positioned(
+      child: Text(
+        upper.toString(),
+        style: TextStyle(fontSize: widgetHeight / 2.9),
+      ),
+      left: widgetHeight / 2.5,
+      top: widgetHeight / 5.7));
+  list.add(Positioned(
+      child: Text(
+        under.toString(),
+        style: TextStyle(fontSize: widgetHeight / 2.9),
+      ),
+      left: widgetHeight / 2.5,
+      top: widgetHeight / 2.35));
+  return list;
+}
+
+//마디 끝 라인 그려주는 함수
+// 0 일반, 1  도돌이표, 2, :곡끝
+List<Widget> drawEndLine(double widgetHeight, double widgetWidth, int type) {
+  List<Widget> list = [];
+  int endLineWidth = type == 0 ? 1 : 2;
+  list.add(Positioned(
+    child: Container(
+      width: widgetHeight * 0.015 * endLineWidth,
+      color: Colors.black,
+    ),
+    height: widgetHeight * 0.51,
+    top: widgetHeight * 0.243,
+    right: 0,
+  ));
+  if (type == 2) {
+    list.add(Positioned(
+      child: Container(
+        width: widgetHeight * 0.015,
+        color: Colors.black,
+      ),
+      height: widgetHeight * 0.51,
+      top: widgetHeight * 0.243,
+      right: widgetHeight * 0.08,
+    ));
+  }
+  return list;
+}
+
+Widget createMadi(Madi madi, double madiWidth) {
+  List<Widget> widgets = [];
+  double widgetHeight = 70; // 디바이스의 크기가져와서 계산 or 고정
+
+  double startPosition = madiWidth * 0.08;
+  double endPosition = madiWidth * 0.95;
+
+  //기본 오선보 삽입
+  widgets.add(Container(
+    width: madiWidth,
+    height: widgetHeight,
+    child: SvgPicture.asset(
+      "assets/base2.svg",
+      fit: BoxFit.fill,
+    ),
+  ));
+
+  // 마디끝 삽입
+  drawEndLine(widgetHeight, madiWidth, madi.endType).forEach((wg) {
+    widgets.add(wg);
+  });
+
+  // 음자리표 삽입
+  if (madi.clef != 0) {
+    widgets.add(drawClef(true, widgetHeight));
+    widgets.add(drawClef(true, widgetHeight));
+    startPosition = startPosition + widgetHeight / 4;
+  }
+  //박자표삽입
+  if (madi.isRhythmShown) {
+    drawRhythem(madi.rhythmUpper, madi.rhythmUnder, widgetHeight).forEach((wg) {
+      widgets.add(wg);
+    });
+    startPosition = startPosition + widgetHeight / 4;
+  }
+
+  if (madi.rhythmUnder == 8) {}
+
+  // 음표삽입
+  var nowPosition = startPosition;
+  // var interval = (endPosition - startPosition) / 4;
+  //박자에 따라 간격 설정
+  var interval = madi.rhythmUnder == 4
+      ? (endPosition - startPosition) / madi.rhythmUpper
+      : (endPosition - startPosition) / madi.rhythmUpper * madi.rhythmUnder / 4;
+  madi.notes.forEach((note) {
+    widgets.add(Positioned(
+      bottom: widgetHeight * pitchParser(note.pitch),
+      left: nowPosition,
+      child: SvgPicture.asset(
+        note.pitch < 71
+            ? note.pitch != 60
+                ? "assets/note${note.leng}.svg"
+                : "assets/note${note.leng}_c.svg"
+            : note.pitch != 60
+                ? "assets/note${note.leng}_2.svg"
+                : "assets/note${note.leng}_2c.svg",
+        color: Colors.black,
+        height: note.leng == 4000 ? widgetHeight / 10 : widgetHeight / 2,
+      ),
+    ));
+    nowPosition = nowPosition + note.leng / 1000 * interval;
+  });
+
+  return Stack(
+    children: widgets,
+  );
+}
